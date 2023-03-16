f7543b0ff863f59b1cc5ae09e44080623a3955ba
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-03-16 15:52:51 -0500

    Añadiendo Feature y step de login

--- Codigo/QA-Auto-RestAssured/src/test/java/runner/TestRunner.java
+++ Codigo/QA-Auto-RestAssured/src/test/java/runner/TestRunner.java
@@ -7,11 +7,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        features = {"src/test/resources/featureFiles/RestAPIReqresDelete.feature"},
+        features = {"src/test/resources/featureFiles/RestAPIReqresDelete.feature",
+                "src/test/resources/featureFiles/RestAPIReqresLogin.feature"},
         glue = {"step.definitions"},
         publish = true
-       // monochrome = true,
+        // monochrome = true,
         //dryRun = false
-       )
+)
 public class TestRunner {
 }
--- Codigo/QA-Auto-RestAssured/src/test/java/step/definitions/RestAPIReqresDeleteTest.java
+++ Codigo/QA-Auto-RestAssured/src/test/java/step/definitions/RestAPIReqresDeleteTest.java
@@ -1,6 +1,5 @@
 package step.definitions;
 
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,41 +11,55 @@ public class RestAPIReqresDeleteTest {
 
     private RequestSpecification request;
     private Response response;
-    private int id;
+    private int userId;
 
     @Given("el administrador esta en la pagina principal")
     public void el_administrador_esta_en_la_pagina_principal() {
         RestAssured.baseURI = "https://reqres.in/api";
         request = RestAssured.given();
+        userId = 2; // ID de un usuario existente
     }
 
     @When("envia una solicitud DELETE")
     public void envia_una_solicitud_DELETE() {
-        id = 2; // Establecer un ID válido y existente aquí
-        response = request.delete("/users/" + id);
+        try {
+            response = request.delete("/users/" + userId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Then("se elimina el usuario")
     public void se_elimina_el_usuario() {
-        assertEquals(204, response.getStatusCode());
+        try {
+            response.then().statusCode(204);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Given("que no existe un usuario")
     public void que_no_existe_un_usuario() {
         RestAssured.baseURI = "https://reqres.in/api";
         request = RestAssured.given();
+        userId = 999999; // ID de un usuario inexistente
     }
 
     @When("envio una solicitud DELETE")
     public void envio_una_solicitud_DELETE() {
-        id = 9999; // Establecer un ID inexistente aquí
-        response = request.delete("/users/" + id);
+        try {
+            response = request.delete("/users/" + userId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Then("la respuesta tiene un codigo de estado no encontrado")
     public void la_respuesta_tiene_un_codigo_de_estado_no_encontrado() {
-        assertEquals(204, response.getStatusCode());
+        try {
+            response.then().statusCode(404);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
-
