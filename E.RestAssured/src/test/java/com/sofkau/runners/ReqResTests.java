a969e75d8026fa9547b8a61f68ce06ac13fc1aea
alejandraguadir <yolima.guadir@correounivalle.edu.co>
2023-03-16 13:39:44 -0500

    Se crea el caso de prueba para validar ligin de usuario con mail incorrecto

---
+++ E.RestAssured/src/test/java/com/sofkau/runners/ReqResTests.java
@@ -0,0 +1,22 @@
+package com.sofkau.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        features = {"src/test/resources/features/rest.feature"},
+        glue = {"com.sofkau.setupdefinitions"},
+        publish = true)
+
+public class ReqResTests {
+
+}
--- E.RestAssured/src/test/java/com/sofkau/setupdefinitions/LoginSetupDefinitions.java
+++ E.RestAssured/src/test/java/com/sofkau/setupdefinitions/LoginSetupDefinitions.java
@@ -1,5 +1,6 @@
 package com.sofkau.setupdefinitions;
 
+import groovy.util.logging.Log;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,9 +8,12 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+import static org.hamcrest.Matchers.notNullValue;
+
 public class LoginSetupDefinitions {
     private String requestBody;
     private Response response;
+    private Log log;
     @Given("que realizo una solicitud para loguearme")
     public void queRealizoUnaSolicitudParaLoguearme() {
         requestBody = "{{\n" +
@@ -19,15 +23,46 @@ public class LoginSetupDefinitions {
     }
     @When("la solicitud es exitosa")
     public void laSolicitudEsExitosa() {
-        response = RestAssured.given().
-                contentType(ContentType.JSON)
+        response = RestAssured.given()
+               .log().all()
+                .contentType(ContentType.JSON)
                 .body(requestBody)
                 .post("https://reqres.in/api/register");
-
 
     }
     @Then("debera mostrar un tocken")
     public void deberaMostrarUnTocken() {
-        System.out.println(response.asString());
+        response.then()
+                .log().all()
+                .statusCode(400)
+                .body("token", notNullValue());
+        //System.out.println(response.asString());
+
     }
+    //Scenario 2
+    @Given("que ingreso una solicitud para loguearme")
+    public void queIngresoUnaSolicitudParaLoguearme() {
+        requestBody = "{{\n" +
+                "    \"email\": \"eve.holtreqres.in\",\n" +
+                "    \"password\": \"pistol\"\n" +
+                "}}";
+    }
+    @When("la solicitud no es exitosa")
+    public void laSolicitudNoEsExitosa() {
+        response = RestAssured.given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post("https://reqres.in/api/register");
+
+    }
+    @Then("debera mostrar un mensaje de error de usuario no encontrado")
+    public void deberaMostrarUnMensajeDeErrorDeUsuarioNoEncontrado() {
+        response.then()
+                .log().all()
+                .statusCode(400)
+                .body("error", notNullValue());
+        //System.out.println(response.asString());
+    }
+
 }
