f1c5eb4b5786538a6d4567c1e5bb1547c9ccb3a7
Bryan Polo <david2315@hotmail.com>
2023-03-16 16:22:44 -0500

    Estructura corregida

---
+++ src/main/java/com/sofkau/service/ServicesPages.java
@@ -0,0 +1,10 @@
+package com.sofkau.service;
+
+import org.openqa.selenium.WebDriver;
+
+public class ServicesPages extends CommonActionOnPages {
+
+    public ServicesPages(WebDriver driver) {
+        super(driver);
+    }
+}
