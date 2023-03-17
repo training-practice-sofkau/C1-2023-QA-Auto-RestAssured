4b23a8a38d90e9a7ca2b8a6dae474c7d567bcee5
Ivan <idruizbe@gmail.com>
2023-03-16 21:59:40 -0500

    Se terminan los dos Step Definition restantes

--- Rest-Assured/src/test/java/com/sofkau/stepdefinition/ServicePostLoginStepDefinition.java
+++ Rest-Assured/src/test/java/com/sofkau/stepdefinition/ServicePostLoginStepDefinition.java
@@ -4,4 +4,17 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.log4j.Logger;
 public class ServicePostLoginStepDefinition {
+    public static Logger LOGGER = Logger.getLogger(String.valueOf(ServicePostLoginStepDefinition.class));
+    @Given("el usuario ingresa su email")
+    public void elUsuarioIngresaSuEmail() {
+    }
+    @Given("el usuario ingresa su email y contrasena")
+    public void elUsuarioIngresaSuEmailYContrasena() {
+    }
+    @Then("recibe un json con el token asignado")
+    public void recibeUnJsonConElTokenAsignado() {
+    }
+    @Then("recibe un json diciendo que falta la contrasena")
+    public void recibeUnJsonDiciendoQueFaltaLaContrasena() {
+    }
 }
--- Rest-Assured/src/test/java/com/sofkau/stepdefinition/ServicePostStepDefinition.java
+++ Rest-Assured/src/test/java/com/sofkau/stepdefinition/ServicePostStepDefinition.java
@@ -4,5 +4,23 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.log4j.Logger;
 public class ServicePostStepDefinition {
-
+    public static Logger LOGGER = Logger.getLogger(String.valueOf(ServicePostStepDefinition.class));
+    @Given("el usuario ingresa su nombre")
+    public void elUsuarioIngresaSuNombre() {
+    }
+    @Given("el usuario ingresa su nobre y trabajo")
+    public void elUsuarioIngresaSuNobreYTrabajo() {
+    }
+    @Given("el usuario ingresa su nobre, trabajo y edad")
+    public void elUsuarioIngresaSuNobreTrabajoYEdad() {
+    }
+    @When("el sistema no coloca la coma al hacer la peticion")
+    public void elSistemaNoColocaLaComaAlHacerLaPeticion() {
+    }
+    @Then("recibe un error de bad request")
+    public void recibeUnErrorDeBadRequest() {
+    }
+    @Then("recibe un json con su informacion registrada")
+    public void recibeUnJsonConSuInformacionRegistrada() {
+    }
 }
