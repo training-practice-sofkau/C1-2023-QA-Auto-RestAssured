9111bd4407fc2a78222990ea95e6323dffdf02bd
Sergio Balcázar Restrepo <91097469+SergioBalca@users.noreply.github.com>
2023-03-24 16:39:04 -0500

    Se elimina carpeta con proyecto intellij con problemas de configuración

--- AutoRestAssured/src/test/java/com/sofkau/setup/ConstantSetup.java
+++
@@ -1,6 +0,0 @@
-package com.sofkau.setup;
-
-public class ConstantSetup {
-    public static final String DELETE_SERVICE_URL = "https://reqres.in/api/users/2";
-    public static final String DELETE_USER_NOT_FOUND = "https://reqres.in/api/users/200";
-}
